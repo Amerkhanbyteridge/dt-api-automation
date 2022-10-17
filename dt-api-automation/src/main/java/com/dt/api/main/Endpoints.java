549e02634df38f5f7853017099cd0cd0a29959de
LAPTOP-NNQF56CK\91914 <vindhya002@gmail.com>
2022-10-17 12:52:37 +0530

    first version of api-automation

---
+++ dt-api-automation/src/main/java/com/dt/api/main/Endpoints.java
@@ -0,0 +1,7 @@
+package com.dt.api.main;
+
+public class Endpoints {
+
+	public static final String baseURI="https://api-qa.dthreaddev.com";
+	public static final String userInvites = "/api/users/invites";
+}
