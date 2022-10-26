b09d06b199e4b296ee48932714b78395abc41116
sivakarthikchinthirla <sivac@byteridge.com>
2022-10-26 11:19:35 +0530

    Adding properties module with all testcases-put/post/get/delete and added testdata

---
+++ dt-api-automation/src/test/java/com/dt/api/properties/get/testGET_issues.java
@@ -0,0 +1,43 @@
+package com.dt.api.properties.get;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.dt.api.main.Config;
+import com.dt.api.main.Endpoints;
+
+import io.restassured.response.Response;
+
+public class testGET_issues {
+
+	JSONObject request = new JSONObject();
+
+	@BeforeClass
+	public void init() throws FileNotFoundException, IOException {
+		request = new JSONObject();
+		baseURI = Endpoints.baseURI;
+
+	}
+
+	@Test
+	public void testGetissues() throws FileNotFoundException, IOException {
+		Response response = given()
+				.headers("Authorization", "bearer " +Config.getToken(), "accept", "application/json").log().all()
+				.get(Endpoints.userdetails, Config.getemailID()).andReturn();
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getTime());
+		System.out.println(response.getBody().toString());
+		System.out.println(response.getStatusLine());
+		System.out.println(response.getHeader("content-type"));
+		assertEquals(response.getStatusCode(), 200);
+	}
+
+}
