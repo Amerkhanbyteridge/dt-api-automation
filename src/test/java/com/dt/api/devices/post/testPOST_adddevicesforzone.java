83e6365fbca96ae95d7b84d3c98a6ef176751a8a
LAPTOP-NNQF56CK\91914 <vindhya002@gmail.com>
2022-11-07 13:43:27 +0530

    corrected syntax issue

--- src/test/java/com/dt/api/devices/post/testPOST_adddevicesforzone.java
+++ src/test/java/com/dt/api/devices/post/testPOST_adddevicesforzone.java
@@ -5,28 +5,26 @@ import static io.restassured.RestAssured.given;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.json.simple.JSONObject;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-<<<<<<< HEAD
-=======
+
 import com.dt.api.baseclass.Baseclass;
->>>>>>> 01779337959a101ea50d1eba4f3ab2fc233e8fb3
+
 import com.dt.api.main.Config;
 import com.dt.api.main.Endpoints;
 
 import io.restassured.http.ContentType;
 
-<<<<<<< HEAD
-public class testPOST_adddevicesforzone {
 
-	JSONObject request = new JSONObject();
-=======
+	
+
 public class testPOST_adddevicesforzone extends Baseclass {
 
->>>>>>> 01779337959a101ea50d1eba4f3ab2fc233e8fb3
+	JSONObject request = new JSONObject();
 	@SuppressWarnings("unchecked")
 	@BeforeClass
 	public void init() throws FileNotFoundException, IOException {
@@ -36,11 +34,11 @@ public class testPOST_adddevicesforzone extends Baseclass {
 		request.put("propertyId", Config.getpropertyID());
 		request.put("protocolId", Config.getprotocolId());
 		baseURI=Endpoints.baseURI;
-<<<<<<< HEAD
+
 		System.out.println(request.toJSONString());	
-=======
+
 		logger.info(request.toJSONString());	
->>>>>>> 01779337959a101ea50d1eba4f3ab2fc233e8fb3
+
 	}
 	
 	@Test
