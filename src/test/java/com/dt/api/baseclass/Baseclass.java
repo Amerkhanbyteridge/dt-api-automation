415243411a0a1d2f5eca3268c682d9f42d57cee3
LAPTOP-NNQF56CK\91914 <vindhya002@gmail.com>
2022-11-10 16:11:24 +0530

    remove unused imports

--- src/test/java/com/dt/api/baseclass/Baseclass.java
+++ src/test/java/com/dt/api/baseclass/Baseclass.java
@@ -11,13 +11,12 @@ import java.lang.reflect.Method;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.simple.JSONObject;
-import org.testng.ITestContext;
+
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
-import com.aventstack.extentreports.Status;
 import com.dt.api.ExtentReports.ExtentManager;
 import com.dt.api.ExtentReports.Reporting;
 import com.dt.api.main.Endpoints;
